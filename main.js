6c8896306dcd1a0a1a2506be2c286f4147b8f965
AndriiKokariev <andrewkok657@gmail.com>
2025-03-03 23:10:41 +0100

    done

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ const modalCloseBtn = document.querySelector(".close-btn");
 const menuOpenBtn = document.querySelector(".menu-open-btn");
 const menuCloseBtn = document.querySelector(".menu-close-btn");
 const mobMenu = document.querySelector(".mob-menu");
+const orderBtn = document.querySelector(".hero-button");
+const form = document.querySelector(".footer-form");
 
 const handleCloseModal = () => {
   backdrop.classList.remove("is-open");
+};
+const handleOpenModal = () => {
+  backdrop.classList.add("is-open");
 };
 const handleCloseMenu = () => {
   mobMenu.classList.remove("is-open");
@@ -15,5 +20,10 @@ const handleOpenMenu = () => {
 };
 
 modalCloseBtn.addEventListener("click", handleCloseModal);
+orderBtn.addEventListener("click", handleOpenModal);
 menuCloseBtn.addEventListener("click", handleCloseMenu);
 menuOpenBtn.addEventListener("click", handleOpenMenu);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  form.reset();
+});
